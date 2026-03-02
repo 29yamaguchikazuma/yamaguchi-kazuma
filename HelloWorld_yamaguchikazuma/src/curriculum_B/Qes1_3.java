671ddede3d8ceb2fd109c62e6d23ef45d7c66b41
yamaguchikazuma <yamaguchikazuma@localhost>
2026-03-02 20:48:23 +0900

    カリキュラムB問題２

--- HelloWorld_yamaguchikazuma/src/curriculum_B/Qes1_3.java
+++ HelloWorld_yamaguchikazuma/src/curriculum_B/Qes1_3.java
@@ -1,40 +1,30 @@
 package curriculum_B;
 
- import java.util.Scanner;
- 
- public class Qes1_3{
-     // 1. コンソールにユーザー名を入力できるようにする。
-     
-     public static void main(String[] argss)  {
-     
-     Scanner sc = new Scanner(System.in);
-    
-   
-      System.out.print("ユーザー名を入力してください：");
-        String userName = sc.nextLine();
+import java.util.Scanner;
 
-        if (userName == null || userName.length() == 0) {
-            System.out.println("名前を入力してください");
+public class Qes1_3 {
+	// 1. コンソールにユーザー名を入力できるようにする。
 
-        } else if (userName.length() > 10) {
-            System.out.println("名前を10文字以内にしてください");
+	public static void main(String[] args) {
 
-        } else {
-            System.out.println("ユーザー名「" + userName + "」を登録しました");
-        }
+		Scanner sc = new Scanner(System.in);
 
-        sc.close();
-            }
-        }    
- 
-    
+		System.out.print("ユーザー名を入力してください：");
+		String userName = sc.nextLine();
 
-        
-       
-     
-    
+		if (userName == null || userName.length() == 0) {
+			System.out.println("名前を入力してください");
 
-     
-     
-    
-    
+		} else if (userName.length() > 10) {
+			System.out.println("名前を10文字以内にしてください");
+
+		} else if (!userName.matches("^[a-zA-Z0-9]+$")) {
+			System.out.println("半角英数字のみで名前を入力してください");
+
+		} else {
+			System.out.println("ユーザー名「" + userName + "」を登録しました");
+		}
+
+		sc.close();
+	}
+}
