0b3873b63dbeddaf555880b4c5cad18502874550
yamaguchikazuma <yamaguchikazuma@localhost>
2026-05-03 22:11:05 +0900

    sample

--- HelloWorld_yamaguchikazuma/src/curriculum_B/Qes5.java
+++ HelloWorld_yamaguchikazuma/src/curriculum_B/Qes5.java
@@ -1,14 +1,18 @@
 package curriculum_B;
 
 public class Qes5 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                System.out.printf("%3d", i * j);
-            }
-            System.out.println();
-        }
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 20; j++) {
+				System.out.printf("%02d * %03d = %03d", i, j, i * j);
 
-    }
+				if (j != 20) {
+					System.out.print(" || ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
 }
