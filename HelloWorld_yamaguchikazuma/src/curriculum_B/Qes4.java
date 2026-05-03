48b05c5561ee3351fe47e41c36a83899cd35b682
yamaguchikazuma <yamaguchikazuma@localhost>
2026-05-03 22:07:11 +0900

    sample　curriculum_B Qes4

--- HelloWorld_yamaguchikazuma/src/curriculum_B/Qes4.java
+++ HelloWorld_yamaguchikazuma/src/curriculum_B/Qes4.java
@@ -1,33 +1,18 @@
 package curriculum_B;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Qes4 {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		Random rand = new Random();
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%02d * %02d = %02d", i, j, i * j);
 
-		while (true) {
-			System.out.println("じゃんけん（0:グー 1:チョキ 2:パー）:");
-			int user = Integer.parseInt(sc.nextLine());
-
-			int enemy = rand.nextInt(3);
-			System.out.println("相手：" + enemy);
-
-			if (user == enemy) {
-				System.out.println("あいこ");
-			} else if ((user == 0 && enemy == 1) ||
-					(user == 1 && enemy == 2) ||
-					(user == 2 && enemy == 0)) {
-				System.out.println("勝ち！");
-				break;
-			} else {
-				System.out.println("負け！");
+				if (j != 9) {
+					System.out.print(" || ");
+				}
 			}
+			System.out.println();
 		}
 
-		sc.close();
 	}
 }
