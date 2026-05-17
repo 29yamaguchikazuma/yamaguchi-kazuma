1f6476e7ccf5397806d8daf961e33987e8f6a9e8
yamaguchikazuma <yamaguchikazuma@localhost>
2026-05-17 23:04:14 +0900

    sample curriculum_B Qes7.java : 問7

---
+++ HelloWorld_yamaguchikazuma/src/curriculum_B/Qes7.java
@@ -0,0 +1,72 @@
+package curriculum_B;
+
+import java.util.Scanner;
+
+public class Qes7 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("生徒の人数を入力してください(2以上): ");
+		int student = Integer.parseInt(sc.nextLine());
+
+		int[][] score = new int[student][4];
+
+		String[] subject = { "英語", "数学", "理科", "社会" };
+
+		//点数入力
+		for (int i = 0; i < student; i++) {
+
+			for (int j = 0; j < 4; j++) {
+
+				System.out.print((i + 1) + "人目の「" + subject[j] + "」の点数を入力してください:");
+
+				score[i][j] = Integer.parseInt(sc.nextLine());
+			}
+
+			System.out.println();
+		}
+
+		//生徒ごとの平均
+		for (int i = 0; i < student; i++) {
+
+			int total = 0;
+
+			for (int j = 0; j < 4; j++) {
+				total += score[i][j];
+			}
+
+			System.out.printf("%d人目の平均点は%.2f点です。", i + 1, (double) total / 4);
+		}
+
+		System.out.println();
+
+		//教科ごとの平均
+		for (int j = 0; j < 4; j++) {
+
+			int total = 0;
+
+			for (int i = 0; i < student; i++) {
+				total += score[i][j];
+			}
+
+			System.out.printf("%sの平均点は%.2f点です。", subject[j], (double) total / student);
+
+		}
+
+		System.out.println();
+
+		//全体平均
+		int allTotal = 0;
+
+		for (int i = 0; i < student; i++) {
+			for (int j = 0; j < 4; j++) {
+				allTotal += score[i][j];
+			}
+		}
+		System.out.printf("全体の平均点は%.2f点です。", (double) allTotal / (student * 4));
+
+		sc.close();
+	}
+
+}
